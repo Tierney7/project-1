b6f384057b3c4d4dfacc7be008597f0475111fb9
tierney7 <tierneybbq@gmail.com>
2022-07-25 21:57:29 -0500

    fancy

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+let map;
+
+function initMap() {
+  const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.getElementById("map"),
+    placeTypePreferences: [
+      { type: "restaurant" },
+      { type: "tourist_attraction" },
+    ],
+    maxPlaceCount: 12,
+  });
+
+  map = localContextMapView.map;
+  map.setOptions({
+    center: { lat: 39.0997, lng: -94.5786 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
