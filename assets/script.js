95f88f7fe32f4d1ee290715bab6b12c1ada657f8
tierney7 <tierneybbq@gmail.com>
2022-08-01 21:24:02 -0500

    cleaner

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,1 @@
 
-
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
- 
-mapboxgl.accessToken = 'pk.eyJ1IjoidGllcmRyb3A3IiwiYSI6ImNsNjVya3M5NzMzczAzam8xMmRuaXQwb2gifQ.BVENabZQ41SpEuvsiOugbQ';
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9, // starting zoom
-projection: 'globe' // display the map as a 3D globe
-});
-map.on('style.load', () => {
-map.setFog({}); // Set the default atmosphere style
-
-
-});
